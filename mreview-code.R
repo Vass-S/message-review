ae755d8fe466192b5a9e761244442197b9590e14
vass87 <vass87@gmail.com>
2016-02-22 12:46:00 -0500

    Updated from Mac. This code used to get the graphs.

--- mreview-code.R
+++ mreview-code.R
@@ -2,42 +2,45 @@
 library(plyr)
 library(stringr)
 library(ggplot2)
-library(wordcloud)
-library(RColorBrewer)
 library(plyr)
 library(stringr)
 rm(list = ls())
 
 score.sentiment = function (sentence, stronger.words, strongest.words)
 {
-  require(stringr)
-  scores = laply(sentence, function (sentence, stronger.words, strongest.words) {
-    sentence = gsub('[[:punct:]]', '', sentence)
-    sentence = gsub('[[:cntrl:]]', '', sentence)
-    sentence = gsub ('\\d+', '', sentence)
-    
-    sentence = tolower(sentence)
-    word.list = str_split(sentence, '\\s+')
-    words = unlist(word.list)
-    stronger.matches = match(words, stronger.words)
-    strongest.matches = match(words, strongest.words)
-    
-    stronger.matches = !is.na(stronger.matches)
-    strongest.matches = !is.na(strongest.matches)
-    score = sum(stronger.matches) - sum(strongest.matches)
-    print(words)
-    return(score)
-  }, stronger.words, strongest.words)
+  #scores = laply(sentences, function (sentence, stronger.words, strongest.words) {
+  sentence = gsub('[[:punct:]]', '', sentence)
+  sentence = gsub('[[:cntrl:]]', '', sentence)
+  sentence = gsub ('\\d+', '', sentence)
   
-  return(scores)
+  sentence = tolower(sentence)
+  word.list = str_split(sentence, '\\s+')
+  words = unlist(word.list)
+  stronger.matches = match(words, stronger.words)
+  strongest.matches = match(words, strongest.words)
+  
+  stronger.matches = !is.na(stronger.matches)
+  strongest.matches = !is.na(strongest.matches)
+  score = sum(stronger.matches) + 2*sum(strongest.matches)
+  return(score)
+  # }, stronger.words, strongest.words)
+  #print(scores)
+  #return(scores)
 }
 
-stronger.words = scan('C:\\Users\\srirams\\Downloads\\test\\Stronger.txt', what='character', comment.char=';')
-strongest.words = scan('C:\\Users\\srirams\\Downloads\\test\\Strongest.txt', what='character', comment.char=';')
+#stronger.words = scan('C:\\Users\\srirams\\Downloads\\test\\Stronger.txt', what='character', comment.char=';')
+#strongest.words = scan('C:\\Users\\srirams\\Downloads\\test\\Strongest.txt', what='character', comment.char=';')
+stronger.words = scan('/Users/sriniwassriram/Downloads/test/Stronger.txt', what='character', comment.char=';')
+strongest.words = scan('/Users/sriniwassriram/Downloads/test/Strongest.txt', what='character', comment.char=';')
 
-sentence <- read.csv ('C:\\Users\\srirams\\Downloads\\test\\Messages.csv')
+
+
+#Messages <- read.csv ('C:\\Users\\srirams\\Downloads\\test\\Messages.csv')
+Messages <- scan('/Users/sriniwassriram/Downloads/test/Messages.txt', what='character', comment.char=';')
 #Messages$x<- as.factor(Messages$x)
 
-MessageResult.scores = score.sentiment(sentence, stronger.words, strongest.words)
-path<-"C:\\Users\\srirams\\Downloads\\test\\"
-write.csv(MessageResult.scores, file=paste(path, "MessageResult.csv",sep=""), row.names=TRUE)
+MessageResult.scores = score.sentiment(Messages, stronger.words, strongest.words)
+#path<-"C:\\Users\\srirams\\Downloads\\test\\"
+path<-"/Users/sriniwassriram/Downloads/test/"
+#write.csv(MessageResult.scores, file=paste(path, "MessageResult.csv",sep=""), row.names=TRUE)
+MessageResult.scores
